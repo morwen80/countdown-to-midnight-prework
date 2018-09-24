2d9cd8e2b55f130e6278e8f857647afa6a0ce879
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-24 10:35:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,7 @@ def countdown(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1 
+  end
 end
 
 
@@ -12,6 +13,7 @@ def countdown_with_sleep(number)
     puts "#{number} SECOND(S)!"
     number -= 1 
     sleep(0.5)
+  end
 
 end
 
